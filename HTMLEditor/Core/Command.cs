441bcfc4d58aed0bb14cd3a7ac4b4751a004a140
vantuz <v.gritskevich25@gmail.com>
2020-03-12 18:30:11 +0300

    moving Command class to Core folder

---
+++ HTMLEditor/Core/Command.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace HTMLEditor.Core
+{
+    public class Command : ICommand
+    {
+        private Action<object> action;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Command(Action<object> action, Func<object, bool> canExecute = null)
+        {
+            this.action = action;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute != null ? canExecute(parameter) : true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (action != null)
+            {
+                action.Invoke(parameter);
+            }
+        }
+    }
+}
--- HTMLEditor/ViewModel/EditorControlViewModel.cs
+++ HTMLEditor/ViewModel/EditorControlViewModel.cs
@@ -4,36 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using HTMLEditor.Core;
 
 namespace HTMLEditor.ViewModel
 {
-    public class Command : ICommand
-    {
-        private Action<object> action;
-        private Func<object, bool> canExecute;
-
-        public event EventHandler CanExecuteChanged;
-
-        public Command(Action<object> action, Func<object, bool> canExecute = null)
-        {
-            this.action = action;
-            this.canExecute = canExecute;
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return canExecute != null ? canExecute(parameter) : true;
-        }
-
-        public void Execute(object parameter)
-        {
-            if (action != null)
-            {
-                action.Invoke(parameter);
-            }
-        }
-    }
-
     public class EditorControlViewModel : BaseViewModel
     {
         private Action<object> textLayoutAction;
@@ -44,7 +18,6 @@ namespace HTMLEditor.ViewModel
                 textLayoutAction = value;
             }
         }
-
 
         public ICommand TextLayoutRedactCommand
         {
